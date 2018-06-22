08ed4f7620197b91b0cd3535e42f597b843d57aa
Nokome Bentley <me@noko.me>
2018-06-22 16:09:13 +1200

    Fix directory structure :wrench:

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 import setuptools
 
-with open('README.md') as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name='nbstencilahostproxy',
     version='0.1.0',
     author='Nokome Bentley',
     description='Proxy to a Stencila Host from a Jupyter Notebook server',
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url='https://github.com/stencila/nbstencilahostproxy',
-    packages=setuptools.find_packages(),
+    packages=['nbstencilahostproxy'],
     keywords=['Jupyter', 'Stencila'],
     classifiers=[
         'Programming Language :: Python',
